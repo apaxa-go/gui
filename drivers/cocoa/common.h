51afe0c8cf53376e1a2e8837e00ac1f51e89c625
unsacrificed <unsacrificed@wwww4.ru>
2018-05-27 05:42:37 +0300

    Update copyright.

--- drivers/cocoa/common.h
+++ drivers/cocoa/common.h
@@ -1,3 +1,7 @@
+// Copyright © 2018 Anton Bekker. All rights reserved.
+// This file is part of the AGui.
+// License information can be found in the LICENSE file.
+
 #ifndef common_h
 #define common_h
 
